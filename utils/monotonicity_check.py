f3ca6502ee24fbd1aa049aef1146a5f81bd809b4
Mohamed Osama Ahmed <mohamed.o.ahmed@borealisai.com>
2021-12-17 01:09:25 +0200

    code clean up

--- utils/monotonicity_check.py
+++ utils/monotonicity_check.py
@@ -1,13 +1,8 @@
 # Copyright (c) 2021-present, Royal Bank of Canada.
-
 # All rights reserved.
-
 #
-
 # This source code is licensed under the license found in the
-
 # LICENSE file in the root directory of this source tree.
-
 #
 
 import torch
--- utils/networks.py
+++ utils/networks.py
@@ -1,21 +1,13 @@
 
 # Copyright (c) 2021-present, Royal Bank of Canada.
-
 # Copyright (c) 2021-present, https://github.com/gnobitab
-
 # All rights reserved.
-
 #
-
 # This source code is licensed under the license found in the
-
 # LICENSE file in the root directory of this source tree.
-
 #####################################################################################
-
 # Code is based on the implementation from CMN:
 # https://github.com/gnobitab/CertifiedMonotonicNetwork
-
 ####################################################################################
 
 import torch
